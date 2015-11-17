ce119ff175b8c558f546b0d859274852eced88dc
knolegsb <seon2you@hotmail.com>
2015-11-17 11:21:50 -0800

    added patient data model

---
+++ TryApi/Models/PatientData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TryApi.Models
+{
+    public class PatientData
+    {
+        public string Name { get; set; }
+        public ICollection<Ailment> Ailment { get; set; }
+        public ICollection<Medication> Medication { get; set; }
+    }
+
+    public class Medication
+    {
+        public string Name { get; set; }
+        public int Doses { get; set; }
+    }
+
+    public class Ailment
+    {
+        public string Name { get; set; }
+    }
+}
