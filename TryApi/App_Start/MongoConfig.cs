ccbde64c460ba3f9a5319d6603ec4112f4a3bcc7
knolegsb <seon2you@hotmail.com>
2015-11-17 13:30:30 -0800

    mongodb config

---
+++ TryApi/App_Start/MongoConfig.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TryApi.Models;
+
+namespace TryApi.App_Start
+{
+    public class MongoConfig
+    {
+        public static void Seed()
+        {
+            var patients = PatientDb.Open();
+        }
+    }
+}
